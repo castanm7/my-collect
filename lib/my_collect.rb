db0011788b1f4015cee70c267baef8b3c80cd2d5
Maria Castaneda <mariamcastaneda29@gmail.com>
2020-10-07 12:12:04 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(array)
+i = 0    
+list_first = []
+    while i < array.size 
+        first_name = yield(array[i])
+                list_first << first_name
+        i += 1
+    end
+    list_first
+end
